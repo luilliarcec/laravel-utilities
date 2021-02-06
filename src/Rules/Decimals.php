f19699b341f034984d9d2a1e4acf04460cf5f6cf
Luis Arce <luilliarcec@gmail.com>
2021-02-06 16:29:19 -0500

    Create rule for validate decimals

---
+++ src/Rules/Decimals.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Luilliarcec\Utilities\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class Decimals implements Rule
+{
+    private string $match;
+    private int $integers;
+    private int $decimals;
+
+    public function __construct(int $integers = 8, int $decimals = 2)
+    {
+        $this->integers = $integers;
+        $this->decimals = $decimals;
+
+        $this->match = sprintf(
+            '/^[\d]{0,%s}(\.[\d]{0,%s})?$/',
+            $this->integers,
+            $this->decimals
+        );
+    }
+
+    public function passes($attribute, $value): bool
+    {
+        if (!is_string($value) && !is_numeric($value)) {
+            return false;
+        }
+
+        return preg_match($this->match, $value) > 0;
+    }
+
+    public function message()
+    {
+        return __('The field :attribute must be a number composed of up to :integers integers and :decimals decimals', [
+            ':integers' => $this->integers,
+            ':decimals' => $this->decimals,
+        ]);
+    }
+}
---
+++ tests/Unit/DecimalsTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Unit;
+
+use Luilliarcec\Utilities\Rules\Decimals;
+use Tests\TestCase;
+
+class DecimalsTest extends TestCase
+{
+    function test_does_not_generate_error_when_the_number_is_a_string()
+    {
+        $validator = $this->app['validator']->make(
+            ['amount' => '123'],
+            ['amount' => new Decimals]
+        );
+
+        $this->assertFalse($validator->fails());
+    }
+
+    function test_does_not_generate_error_when_number_is_0()
+    {
+        $validator = $this->app['validator']->make(
+            ['amount' => '0'],
+            ['amount' => new Decimals]
+        );
+
+        $this->assertFalse($validator->fails());
+    }
+
+    function test_does_not_generate_error_when_number_is_whitespace()
+    {
+        $validator = $this->app['validator']->make(
+            ['amount' => ''],
+            ['amount' => new Decimals]
+        );
+
+        $this->assertFalse($validator->fails());
+    }
+
+    function test_null_generates_an_error()
+    {
+        $validator = $this->app['validator']->make(
+            ['amount' => null],
+            ['amount' => new Decimals]
+        );
+
+        $this->assertTrue($validator->fails());
+    }
+
+    function test_anything_that_does_not_have_a_number_format_generates_an_error()
+    {
+        $validator = $this->app['validator']->make(
+            ['amount' => 'asas'],
+            ['amount' => new Decimals]
+        );
+
+        $this->assertTrue($validator->fails());
+    }
+
+    function test_when_the_number_has_more_integers_than_allowed_it_generates_an_error()
+    {
+        $validator = $this->app['validator']->make(
+            ['amount' => 12345],
+            ['amount' => new Decimals(4,2)]
+        );
+
+        $this->assertTrue($validator->fails());
+    }
+
+    function test_when_the_number_has_more_decimals_than_allowed_it_generates_an_error()
+    {
+        $validator = $this->app['validator']->make(
+            ['amount' => 0.25],
+            ['amount' => new Decimals(1,1)]
+        );
+
+        $this->assertTrue($validator->fails());
+    }
+}
