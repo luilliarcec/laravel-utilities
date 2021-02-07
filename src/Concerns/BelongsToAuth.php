f7e20af0af6db6afcc542682c95545db36cf5614
Luis Arce <luilliarcec@gmail.com>
2021-02-07 00:14:04 -0500

    Clean Code

--- src/Concerns/BelongsToAuth.php
+++ src/Concerns/BelongsToAuth.php
@@ -12,20 +12,18 @@ trait BelongsToAuth
 {
     public static string $authIdColumn = 'user_id';
 
-    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
-    {
-        return $this->belongsTo(config('auth.providers.users.model'), self::$authIdColumn);
-    }
-
     public static function bootBelongsToAuth()
     {
         static::creating(function ($model) {
-            if (Auth::check()) {
-                $model->user()->associate(Auth::user());
-            }
+            $model->user()->associate(Auth::user());
         });
 
         static::addGlobalScope(new AuthScope);
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(config('auth.providers.users.model'), self::$authIdColumn);
     }
 
     public function getQualifiedAuthIdColumn(): string
