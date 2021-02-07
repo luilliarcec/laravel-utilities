a0f4cc0f08417c3f3bc2b16b818ec11778e9c7f7
Luis Arce <luilliarcec@gmail.com>
2021-02-07 00:08:07 -0500

    Fixed Trait BelognsToAuth

--- src/Concerns/BelongsToAuth.php
+++ src/Concerns/BelongsToAuth.php
@@ -5,19 +5,19 @@ namespace Luilliarcec\Utilities\Concerns;
 use Illuminate\Support\Facades\Auth;
 use Luilliarcec\Utilities\Scopes\AuthScope;
 
+/**
+ * @method static static|\Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder withoutAuth()
+ */
 trait BelongsToAuth
 {
     public static string $authIdColumn = 'user_id';
 
-    public function user()
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
-        return $this->belongsTo(
-            config('auth.providers.users.model'),
-            self::$authIdColumn
-        )->withTrashed();
+        return $this->belongsTo(config('auth.providers.users.model'), self::$authIdColumn);
     }
 
-    public static function bootHasCompany()
+    public static function bootBelongsToAuth()
     {
         static::creating(function ($model) {
             if (Auth::check()) {
@@ -25,7 +25,7 @@ trait BelongsToAuth
             }
         });
 
-        static::addGlobalScope(new AuthScope());
+        static::addGlobalScope(new AuthScope);
     }
 
     public function getQualifiedAuthIdColumn(): string
