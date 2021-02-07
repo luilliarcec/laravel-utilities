6135f40a98527d6081800f7d93ccc87e9db5efd4
Luis Arce <luilliarcec@gmail.com>
2021-02-07 18:04:39 -0500

    Tests Auth

---
+++ tests/Unit/AuthValidationsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Luilliarcec\Utilities\Rules\Auth as AuthRules;
+use Tests\TestCase;
+use Tests\Utils\Invoice;
+use Tests\Utils\User;
+
+class AuthValidationsTest extends TestCase
+{
+    function test_a()
+    {
+        $this->actingAs(User::create(['name' => 'luis arce']));
+
+        Invoice::create(['description' => 'Invoice 1']);
+
+        $validator = $this->app['validator']->make(
+            ['invoice_id' => 1],
+            ['invoice_id' => AuthRules::exists('invoices', 'id')]
+        );
+
+        $this->assertFalse($validator->fails());
+    }
+}
---
+++ tests/Unit/BelongsToAuthTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+use Tests\Utils\Invoice;
+use Tests\Utils\User;
+
+class BelongsToAuthTest extends TestCase
+{
+    function test_get_the_table_name_and_column_name_of_the_relation()
+    {
+        $this->assertEquals(
+            'invoices.user_id',
+            (new Invoice())->getQualifiedAuthIdColumn()
+        );
+    }
+
+    function test_gets_a_belongs_to_instance()
+    {
+        $this->assertInstanceOf(
+            \Illuminate\Database\Eloquent\Relations\BelongsTo::class,
+            (new Invoice())->user()
+        );
+    }
+
+    function test_the_authenticated_user_is_saved_by_event()
+    {
+        $this->actingAs(User::create(['name' => 'luis arce']));
+
+        $invoice = Invoice::create(['description' => 'Invoice 1']);
+
+        $this->assertEquals(Auth::id(), $invoice->user_id);
+    }
+
+    function test_get_the_relationship_with_the_authenticated_user()
+    {
+        $this->actingAs(User::create(['name' => 'luis arce']));
+
+        $invoice = Invoice::create(['description' => 'Invoice 1']);
+
+        $this->assertInstanceOf(User::class, $invoice->user);
+    }
+
+    function test_the_global_scope_of_get_by_authenticated_user_applies()
+    {
+        (new Invoice())
+            ->user()->associate(User::create(['name' => 'andres cardenas']))
+            ->fill(['description' => 'Invoice 1'])
+            ->saveQuietly();
+
+        $this->actingAs(User::create(['name' => 'luis arce']));
+        Invoice::create(['description' => 'Invoice 2']);
+
+        $this->assertTrue(Invoice::all()->contains('description', 'Invoice 2'));
+        $this->assertFalse(Invoice::all()->contains('description', 'Invoice 1'));
+    }
+}
