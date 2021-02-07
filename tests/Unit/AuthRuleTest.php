d8e1bc624fe426e0135a7f83026e35acd7728808
Luis Arce <luilliarcec@gmail.com>
2021-02-07 18:46:26 -0500

    Validation fails when record does not belong to authenticated user

--- tests/Unit/AuthRuleTest.php
+++ tests/Unit/AuthRuleTest.php
@@ -9,10 +9,9 @@ use Tests\Utils\User;
 
 class AuthRuleTest extends TestCase
 {
-    function test_a()
+    function test_validates_that_the_record_exists_with_the_authenticated_user()
     {
         $this->actingAs(User::create(['name' => 'luis arce']));
-
         Invoice::create(['description' => 'Invoice 1']);
 
         $validator = $this->app['validator']->make(
@@ -21,5 +20,23 @@ class AuthRuleTest extends TestCase
         );
 
         $this->assertFalse($validator->fails());
+    }
+
+    function test_validation_fails_when_record_does_not_belong_to_authenticated_user()
+    {
+        (new Invoice())
+            ->user()->associate(User::create(['name' => 'andres cardenas']))
+            ->fill(['description' => 'Invoice 1'])
+            ->saveQuietly();
+
+        $this->actingAs(User::create(['name' => 'luis arce']));
+        Invoice::create(['description' => 'Invoice 2']);
+
+        $validator = $this->app['validator']->make(
+            ['invoice_id' => 1],
+            ['invoice_id' => AuthRules::exists('invoices', 'id')]
+        );
+
+        $this->assertTrue($validator->fails());
     }
 }
