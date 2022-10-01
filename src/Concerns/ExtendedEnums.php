a4e41a557d0425967eb0c10a8e5f4dea22c9da1b
Luis Andrés Arce C <luilliarcec@gmail.com>
2022-09-30 19:01:41 -0500

    2.x Extended Enums (#5)
    * update composer and gitignore
    
    * rm composer.lock
    
    * Rename concern ExtendedEnums.php

---
+++ src/Concerns/ExtendedEnums.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Luilliarcec\Utilities\Concerns;
+
+use Illuminate\Support\Str;
+
+trait ExtendedEnums
+{
+    public static function toArray(): array
+    {
+        foreach (self::cases() as $enum) {
+            $values[$enum->value ?? $enum->name] = self::cast($enum->name);
+        }
+
+        return $values ?? [];
+    }
+
+    public function title(): string
+    {
+        return self::cast($this->name);
+    }
+
+    protected static function cast(mixed $name = null): string
+    {
+        return trans()->has($key = "validation.attributes.{$name}")
+            ? ucfirst(__($key))
+            : (string)Str::of($name)->headline()->ucfirst();
+    }
+}
